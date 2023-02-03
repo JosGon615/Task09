5906ff4c4c0d7d63e8203c98e747d6ed05c1e1f3
PabloAlonsoGarcia <pabalo172@alu.centroafuera.es>
2023-02-03 10:56:12 +0100

    Button map created

--- app/src/main/java/com/example/task09fulllayout/MainActivity.kt
+++ app/src/main/java/com/example/task09fulllayout/MainActivity.kt
@@ -1,13 +1,22 @@
 package com.example.task09fulllayout
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.task09fulllayout.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+
+        binding.mapa.setOnClickListener {
+            var intent = Intent(this, MapsActivity::class.java)
+            startActivity(intent)
+        }
 
 
 
